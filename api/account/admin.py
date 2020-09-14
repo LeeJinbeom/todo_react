ccefa1e85a6187ad303d27c25f5fec6a05464e99
LeeJinbeom <jinbomang@gmail.com>
2020-09-14 15:26:29 +0900

    초반세팅완료

---
+++ api/account/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User
+
+# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','email','phone_number']
+    list_display_links = ['username','email','phone_number']
